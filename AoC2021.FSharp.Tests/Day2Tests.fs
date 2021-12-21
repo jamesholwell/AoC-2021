3de4c3ec424c6e603d4c416be29797e35a9d2af0
James Holwell <james@polylytics.com>
2021-12-21 12:48:15 +0000

    day 2 part 1

---
+++ AoC2021.FSharp.Tests/Day2Tests.fs
@@ -0,0 +1,21 @@
+module Day2Tests
+
+open AoC2021.Core
+open AoC2021.FSharp
+open Xunit
+
+let exampleInput = @"
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+"
+
+[<Fact>]
+let ``Solves Part One Example`` () =
+    let solver = upcast new Day2() : ISolver
+    let actual = solver.SolvePartOne exampleInput
+    Assert.Equal("150", actual)
+    
---
+++ AoC2021.FSharp/Day2.fs
@@ -0,0 +1,27 @@
+﻿namespace AoC2021.FSharp
+
+open AoC2021.Core
+
+module Day2 =
+    let (|Prefix|_|) (p: string) (s: string) =
+        if s.StartsWith p then Some (s.Substring(p.Length) |> int)
+        else None
+    
+    let UpdateVector = fun (h, d) s ->
+        match s with
+        | Prefix "forward" x -> (h + x, d)
+        | Prefix "down"    x -> (h, d + x)
+        | Prefix "up"      x -> (h, d - x)
+        | _                  -> (h, d)
+    
+    let GetMagnitude = fun (h, d) -> h * d
+            
+    let Solve =
+        Shared.Split >>
+        Seq.fold UpdateVector (0,0) >>
+        GetMagnitude
+
+type Day2() =
+    interface ISolver with
+        member this.SolvePartOne(s: string) = Day2.Solve s |> string
+        member this.SolvePartTwo(s: string) = ""
