298e5d84eac48be1d70ba5e020b4ca28dbdde613
James Holwell <james@polylytics.com>
2021-12-21 12:56:16 +0000

    day 2 part 2

--- AoC2021.FSharp/Day2.fs
+++ AoC2021.FSharp/Day2.fs
@@ -18,12 +18,27 @@ module Day2 =
             | Prefix "up"      x -> (h, d - x)
             | _                  -> (h, d)
 
+    let UpdateAimedVector =
+        fun (h, d, a) s ->
+            match s with
+            | Prefix "forward" x -> (h + x, d + x * a, a)
+            | Prefix "down"    x -> (h, d, a + x)
+            | Prefix "up"      x -> (h, d, a - x)
+            | _                  -> (h, d, a)
+        
     let GetMagnitude = fun (h, d) -> h * d
+
+    let GetMagnitude3 (h, d, _) = GetMagnitude (h, d)
 
     let Solve =
         Shared.Split
         >> Seq.fold UpdateVector (0, 0)
         >> GetMagnitude
+
+    let SolveAimed =
+        Shared.Split
+        >> Seq.fold UpdateAimedVector (0, 0, 0)
+        >> GetMagnitude3
 
     let exampleInput = @"
 forward 5
@@ -38,8 +53,13 @@ forward 2
     let ``Solves Part One Example`` () =
         let actual = Solve exampleInput
         Assert.Equal(150, actual)
-
+        
+    [<Fact>]
+    let ``Solves Part Two Example`` () =
+        let actual = SolveAimed exampleInput
+        Assert.Equal(900, actual)
+        
 type Day2(input: string) =
     interface ISolver with
         member this.SolvePartOne() = Day2.Solve input
-        member this.SolvePartTwo() = failwith "NotImplemented"
+        member this.SolvePartTwo() = Day2.SolveAimed input
