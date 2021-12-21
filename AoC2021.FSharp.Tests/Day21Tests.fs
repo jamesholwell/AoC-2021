0beb621e602b656b3187bd46535c83c759d8331a
James Holwell <james@abstractleap.com>
2021-12-21 23:46:47 +0000

    day 21 part 1

---
+++ AoC2021.FSharp.Tests/Day21Tests.fs
@@ -0,0 +1,44 @@
+module Day21Tests
+
+open AoC2021.Core
+open AoC2021.FSharp
+open AoC2021.FSharp.Day21
+open Xunit
+
+let exampleInput = @"
+Player 1 starting position: 4
+Player 2 starting position: 8
+"
+
+[<Fact>]
+let ``Parser reads input as expected`` () =
+    let player1Position, player2Position = Parse exampleInput
+    Assert.Equal(4, player1Position)
+    Assert.Equal(8, player2Position)
+    
+[<Fact>]
+let ``Deterministic die rolls as expected`` () =
+    let s = Day21.DeterministicDie 100
+    Assert.Equal(1, Seq.item 0 s)
+    Assert.Equal(100, Seq.item 99 s)
+    Assert.Equal(1, Seq.item 100 s)
+    Assert.Equal(50, Seq.item 149 s)
+    
+[<Fact>]
+let ``Roller scores as expected`` () =
+    let s = DeterministicDie 100 |> DieRoller 3
+    Assert.Equal(1 + 2 + 3, Seq.item 0 s)
+    Assert.Equal(4 + 5 + 6, Seq.item 1 s)
+    
+[<Fact>]
+let ``Turn taker behaves as expected`` () =
+    let rules = { BoardSize = 10; Die = seq { 0 }; NumberOfRolls = 3; TargetScore = 1000 }
+    let state = { Name = "Test"; Location = 4; Score = 0; Rolls = 0 }
+    Assert.Equal({ Name = "Test"; Location = 7; Score = 7; Rolls = 3 }, TakeTurn rules state 3)
+    Assert.Equal({ Name = "Test"; Location = 1; Score = 1; Rolls = 3 }, TakeTurn rules state 7)
+
+[<Fact>]
+let ``Solves Part One Example`` () =
+    let solver = upcast new Day21() : ISolver
+    let actual = solver.SolvePartOne exampleInput
+    Assert.Equal("739785", actual)
---
+++ AoC2021.FSharp/Day21.fs
@@ -0,0 +1,71 @@
+﻿namespace AoC2021.FSharp
+
+open AoC2021.Core
+
+module Day21 =
+    type GameRules = {
+        BoardSize: int
+        Die: seq<int>
+        NumberOfRolls: int
+        TargetScore: int
+    }
+    
+    type PlayerState = {
+        Name: string
+        Location: int // 1-indexed
+        Score: int
+        Rolls: int
+    }
+    
+    let Parse s =
+        let positions = Shared.Split s |> Array.map (fun s -> s.Substring(s.Length - 2) |> int) 
+        (positions[0], positions[1])
+        
+    let DeterministicDie sides =
+        Seq.initInfinite (fun x -> 1 + x % sides)
+        
+    let DieRoller n (die : seq<int>)  =
+        die |> Seq.chunkBySize n |> Seq.map Array.sum
+        
+    let TakeTurn rules state roll =
+        let newLocation = (state.Location - 1 + roll) % rules.BoardSize + 1
+        {
+            Name = state.Name
+            Location = newLocation
+            Score = state.Score + newLocation
+            Rolls = state.Rolls + rules.NumberOfRolls 
+        }
+    
+    let PlayGame rules player1 player2 =
+        let roller = rules.Die |> DieRoller rules.NumberOfRolls
+        let rec loop roller thisPlayer otherPlayer =
+            let roll = Seq.head roller
+            let newState = TakeTurn rules thisPlayer roll
+            if (newState.Score >= rules.TargetScore) then
+                rules, newState, otherPlayer
+            else
+                loop (Seq.tail roller) otherPlayer newState
+        loop roller player1 player2
+    
+    let PlayPartOne (pl1, pl2) =
+        let rules = {
+            BoardSize = 10
+            Die = DeterministicDie 100
+            NumberOfRolls = 3
+            TargetScore = 1000
+        }
+        let player1 = { Name = "Player 1"; Location = pl1 ; Score = 0 ; Rolls = 0 }
+        let player2 = { Name = "Player 2"; Location = pl2 ; Score = 0 ; Rolls = 0 }
+        PlayGame rules player1 player2
+    
+    let CalculateScore (rules, player1, player2) =
+        let rolls = player1.Rolls + player2.Rolls
+        let loser = if player1.Score >= rules.TargetScore then player2 else player1
+        loser.Score * rolls
+    
+type Day21() =
+    interface ISolver with
+        member this.SolvePartOne(s: string) =
+            Day21.Parse s |> Day21.PlayPartOne |> Day21.CalculateScore |> string
+            
+        member this.SolvePartTwo(s: string) = ""
